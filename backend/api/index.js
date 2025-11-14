557c8713ed152407b395e36841dee95df1c96946
rhapsodicpug <arya.manjardekar@gmail.com>
2025-11-14 17:11:01 +0530

    Simplify Vercel handler: Export Express app directly

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,5 @@
 import app from '../server.js';
 
-// Vercel serverless function handler
-// Export both default and named export for compatibility
-const handler = (req, res) => {
-  return app(req, res);
-};
-
-export default handler;
-export { handler };
+// Export Express app directly for Vercel serverless functions
+export default app;
 
